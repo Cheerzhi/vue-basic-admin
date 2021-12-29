2a9171d2124a4a1026b44c19a4ea269000bf3d27
梁大志 <liangdazhi@liangdazhideMacBook-Pro.local>
2021-12-29 16:28:14 +0800

    初始化项目

---
+++ src/store/modules/user.js
@@ -0,0 +1,113 @@
+import {
+  login,
+  getInfo
+} from '@/api/user'
+import {
+  getToken,
+  setToken,
+  removeToken
+} from '@/utils/auth'
+import {
+  resetRouter
+} from '@/router'
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: ''
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  }
+}
+
+const actions = {
+  // 用户登陆
+  login({
+    commit
+  }, userInfo) {
+    const {
+      username,
+      password
+    } = userInfo
+    return new Promise((resolve, reject) => {
+      login({
+        username: username.trim(),
+        password: password
+      }).then(res => {
+        const {
+          data
+        } = res
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // 获取用户信息及菜单处理
+  getInfo({
+    commit,
+    state
+  }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token).then(response => {
+        const {
+          data
+        } = response
+
+        if (!data) {
+          return reject('Verification failed, please Login again.')
+        }
+        const {
+          name,
+          avatar
+        } = data
+        commit('SET_NAME', name)
+        commit('SET_AVATAR', avatar)
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // token刷新的接口
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      resolve()
+    })
+  },
+  // 退出登陆时操作
+  logout({ commit }) {
+    return new Promise((resolve, reject) => {
+      commit('SET_TOKEN', '')
+      removeToken()
+      resetRouter()
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
